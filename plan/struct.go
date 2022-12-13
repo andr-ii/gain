989555ff708e94bb5c439c21a9211750f067cdd6
andr_ll <andr.lyt.dev@gmail.com>
2022-12-13 16:52:36 +0100

    feat: add result struct and update plan struct

--- plan/struct.go
+++ plan/struct.go
@@ -9,15 +9,27 @@ type Plan struct {
 }
 
 type request struct {
+	incremental
+
 	Amount uint8 `json:"amount"` // An amount of requests to be performed.
-	RPS    uint8 `json:"rps"`    // Requests per second.
+	RPS    rps   `json:"rps"`    // Requests per second options.
 	Body   body  `json:"body"`   // Request body options.
 }
 
+type rps struct {
+	incremental
+	Max      *uint16 `json:"max_rps"`  // A maximum amount of RPS. Depends on 'Incr'.
+	Value    uint16  `json:"value"`    // A default value of RPS.
+	Interval uint16  `json:"interval"` // An interval in which PRS value should be increased.
+}
 type body struct {
-	Value  string `json:"value"`     // A default value of request's 'body', 'parameter' or 'query'. Must be a 'string' type.
-	Type   string `json:"type"`      // A type of request 'body', 'parameter' or 'query'. Can be 'number' or 'string' so far.
-	Format string `json:"format"`    // A 'body', 'parameter' or 'query' value.
-	Incr   *bool  `json:"increment"` // Describes tha body has to be incremented or no.
-	Step   *uint  `json:"step"`      // A value which has to be added to default on each iteration. Depends on 'Incr'.
+	incremental
+	Value  uint16 `json:"value"`  // A default value of request's 'body', 'parameter' or 'query'. Must be a 'string' type.
+	Type   string `json:"type"`   // A type of request 'body', 'parameter' or 'query'. Can be 'number' or 'string' so far.
+	Format string `json:"format"` // A 'body', 'parameter' or 'query' value.
+}
+
+type incremental struct {
+	Incr *bool `json:"increment"` // Describes tha body has to be incremented or no.
+	Step *uint `json:"step"`      // A value which has to be added to default on each iteration. Depends on 'Incr'.
 }
---
+++ result/struct.go
@@ -0,0 +1,7 @@
+package result
+
+type ResultMetrics struct {
+	RequestId uint64
+	Latency   float32
+	Status    string
+}
