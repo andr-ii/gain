edb83182e5d3d1dd20b8cd3aa818f3e7dfa3f717
andr_ll <andr.lyt.dev@gmail.com>
2022-12-21 17:31:10 +0100

    fix: remove slice for bars render in progress.go

--- metrics/progress.go
+++ metrics/progress.go
@@ -4,24 +4,22 @@ import (
 	"andr-ll/plt/colors"
 	"andr-ll/plt/conf"
 	"andr-ll/plt/terminal"
-	"strings"
 	"time"
 )
 
-var state_done = "█"
+var state_done = colors.Blue("█")
 var progress_label_raw = "[ PROGRESS: "
 var progress_label = colors.Blue(progress_label_raw)
 var progress_row = statistics_height + 2
 
 func runProgress() {
-	bar := []string{}
-	rawLabelLen := len(progress_label_raw) + 1
+	progressCol := len(progress_label_raw) + 1
 	terminal.PrintAt(progress_row, 0, progress_label)
 	terminal.PrintAt(progress_row, len(progress_label_raw)+80, colors.Blue(" ]"))
 
 	for {
 		time.Sleep(time.Duration(conf.Plan.Duration*750) * time.Millisecond)
-		bar = append(bar, state_done)
-		terminal.PrintAt(progress_row, rawLabelLen, strings.Join(bar, ""))
+		terminal.PrintAt(progress_row, progressCol, state_done)
+		progressCol += 1
 	}
 }
